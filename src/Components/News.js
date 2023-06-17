f322a08cb5e09c01e575d92784babd392ad024ef
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2023-06-17 21:03:53 +0500

    video 28 completed

--- src/Components/News.js
+++ src/Components/News.js
@@ -6,17 +6,45 @@ export class News extends Component {
             super();
             this.state={
                 articles: [],
-                loading: false
+                loading: false,
+                page:1,
+                totalResults:2
             }
+        }
+        handleNextPage= async()=>{
+          if(this.state.page+1>Math.ceil(this.state.totalResults/20))
+          {
+
+          }
+          else
+          {
+            let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=5371b94aa0f14a2790598db6d4898226&page=${this.state.page+1}`
+            let data = await fetch(url);
+            let parsedData = await data.json(data);
+          this.setState({ 
+            page:this.state.page +1,
+            articles: parsedData.articles
+          })
+        }
+        }
+        handlePreviousPage= async()=>{
+          let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=5371b94aa0f14a2790598db6d4898226&page=${this.state.page-1}&pageSize=20`
+            let data = await fetch(url);
+            let parsedData = await data.json(data);
+          this.setState({ 
+            page:this.state.page -1,
+            articles: parsedData.articles
+          })
+
         }
          async componentDidMount(){
             
-            let url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=5371b94aa0f14a2790598db6d4898226"
+            let url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=5371b94aa0f14a2790598db6d4898226&page=1&pageSize=20"
             let data = await fetch(url);
             let parsedData = await data.json(data);
            
             
-            this.setState({articles: parsedData.articles});
+            this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults});
 
         }
   render() {
@@ -29,12 +57,17 @@ export class News extends Component {
                 <NewsItem  title={ element.title? element.title.slice(0.45):""} description={element.description? element.description.slice(0,85):""} imageUrl={element.urlToImage}  newsUrl={element.url}/>
                 </div>
 
+
             })}
             
 
             
         </div>
-        
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark  " onClick={this.handlePreviousPage}> {'\u2190'} Previous</button>
+        <button type="button" class="btn btn-dark  "onClick={this.handleNextPage}>Next {'\u2192'}</button>
+        </div>
+
         
         </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -4,13 +4,14 @@ export class NewsItem extends Component {
   render() {
     let {title, description,imageUrl,newsUrl} = this.props;
     return (
+      
       <div className='my-3'>
        <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl? "https://cdn.arstechnica.net/wp-content/uploads/2018/07/180711_MacBookPro_NewYorkCity_BK_41579-760x380.jpg":imageUrl} className="card-img-top" alt="..."/>
          <div className="card-body">
          <h5 className="card-title">{title}...</h5>
         <p className="card-text">{description}</p>
-        <a href={newsUrl}target="_blank" className="btn btn-sm btn-primary">Read More</a>
+        <a rel='noreferrer' href={newsUrl}target="_blank" className="btn btn-sm btn-dark">Read More</a>
     </div>
 </div>
         
