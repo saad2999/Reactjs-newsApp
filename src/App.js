576395dc6cc2bb80d2ceff5eb6df5f0620470998
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2023-06-23 18:45:29 +0500

    video 36 completed top loading bar added

--- src/App.js
+++ src/App.js
@@ -5,27 +5,40 @@ import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
 
 
 export default class App extends Component {
 
- 
+ state={
+  progress: 0,
+ }
+ setProgress=(progress)=> {
+  this.setState({ progress: progress});
+ }
   render() {
     
     return (
       <div>
          <Router>
        <Navbar/>
+       <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={this.state.progress}
+        
+      />
        
        <Routes> 
        {/* <News pageSize={5} Country='us' category='sports'/> */}
-       <Route  exect path="/" element={<News  key="general" pageSize={5} Country='us' category='general'/>} />
-       <Route  exect path="/technology"  element={<News key="technology" pageSize={5} Country='us' category='technology'/>} />
-       <Route  exect path="/science"  element={<News key="science" pageSize={5} Country='us' category='science'/>} />
-       <Route  exect path="/sports"  element={<News key="sports" pageSize={5} Country='us' category='sports'/>} />
-       <Route  exect path="/business"  element={<News key="business" pageSize={5} Country='us' category='business'/>} />
-       <Route  exect path="/entertainment" key="entertainment" element={<News key="entertainment" pageSize={5} Country='us' category='entertainment'/>} />
-       <Route  exect path="/health"  element={<News key="health" pageSize={5} Country='us' category='health'/>} />
+       <Route  exect path="/" element={<News  key="general" setProgress={this.setProgress} pageSize={5} Country='us' category='general'/>} />
+       <Route  exect path="/technology"  element={<News setProgress={this.setProgress} key="technology" pageSize={5} Country='us' category='technology'/>} />
+       <Route  exect path="/science"  element={<News setProgress={this.setProgress} key="science" pageSize={5} Country='us' category='science'/>} />
+       <Route  exect path="/sports"  element={<News setProgress={this.setProgress} key="sports" pageSize={5} Country='us' category='sports'/>} />
+       <Route  exect path="/business"  element={<News setProgress={this.setProgress} key="business" pageSize={5} Country='us' category='business'/>} />
+       <Route  exect path="/entertainment" key="entertainment" element={<News setProgress={this.setProgress} key="entertainment" pageSize={5} Country='us' category='entertainment'/>} />
+       <Route  exect path="/health"  element={<News setProgress={this.setProgress} key="health" pageSize={5} Country='us' category='health'/>} />
        </Routes>
 
        </Router>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,9 +7,17 @@ export class NewsItem extends Component {
       
       <div className='my-3'>
        <div className="card" >
-       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left: '90%',zIndex:1}}>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          position: 'absolute',
+          right:0
+        }}>
+        <span className="badge rounded-pill bg-danger" >
         {source}
         </span>
+        </div>
+       
         <img src={!imageUrl? "https://cdn.arstechnica.net/wp-content/uploads/2018/07/180711_MacBookPro_NewYorkCity_BK_41579-760x380.jpg":imageUrl} className="card-img-top" alt="..."/>
          <div className="card-body">
          <h5 className="card-title">{title}...</h5>
